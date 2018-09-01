d9bd621efee6d36ddb5abfe7a0a7ca5e8bcce169
Pavel Zavoiskikh <hapass@ya.ru>
2018-09-02 01:35:26 +0300

    Append rename symbol to filtered renamed file name for cut.

--- sort.sh
+++ sort.sh
@@ -1,2 +1,2 @@
 git add .
-git status -s | sed -e 's/\(.*\) -> \(.*\)/\2/g' | grep .*hx$ | cut -c4- | python sort.py
+git status -s | sed -e 's/\(.*\) -> \(.*\)/R  \2/g' | grep .*hx$ | cut -c4- | python sort.py
